23ecb89d2e0cafdb55ee1c0cec3d8ad2ad8384bc
Prime <sanjay22rajpoot@gmail.com>
2022-10-08 23:55:22 +0530

    Code fix and new settings tab
    Code fix and new settings tab

--- password.php
+++ password.php
@@ -71,6 +71,7 @@
 					<div class="drop-inner">
 						<ul>
 							<li><a href="<?php echo BASE_URL.$user->username; ?>"><?php echo $user->username; ?></a></li>
+							<li><a href="<?php echo BASE_URL;?>settings/account">Settings2</a></li>
 							<li><a href="<?php echo BASE_URL;?>settings/account">Settings</a></li>
 							<li><a href="<?php echo BASE_URL;?>includes/logout.php">Log out</a></li>
 						</ul>
@@ -92,21 +93,6 @@
 
 		<div class="lefter">
 			<div class="inner-lefter">
-
-				<div class="acc-info-wrap">
-					<div class="acc-info-bg">
-						<!-- PROFILE-COVER -->
-						<img src="<?php echo BASE_URL.$user->profileCover; ?>"/>  
-					</div>
-					<div class="acc-info-img">
-						<!-- PROFILE-IMAGE -->
-						<img src="<?php echo BASE_URL.$user->profileImage; ?>"/>
-					</div>
-					<div class="acc-info-name">
-						<h3><?php echo $user->screenName; ?></h3>
-						<span><a href="<?php echo BASE_URL.$user->username; ?>">@<?php echo $user->username; ?></a></span>
-					</div>
-				</div><!--Acc info wrap end-->
 
 				<div class="option-box">
 					<ul> 
